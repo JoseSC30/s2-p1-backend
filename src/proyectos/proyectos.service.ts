088def96c5d566bbe47645858c528298ea08ccce
jose luis sossa castro <joseluis.universidad2020@gmail.com>
2025-04-26 21:31:42 -0400

    Segundo cambio al service de Proyecto - 01

--- src/proyectos/proyectos.service.ts
+++ src/proyectos/proyectos.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProyectosService {}
+export class ProyectosService {
+    getProyectos() {
+        return [
+        {
+            id: 1,
+            nombre: 'Proyecto 1',
+            descripcion: 'Descripción del proyecto 1',
+            fechaInicio: new Date('2023-01-01'),
+            fechaFin: new Date('2023-12-31'),
+        },
+        {
+            id: 2,
+            nombre: 'Proyecto 2',
+            descripcion: 'Descripción del proyecto 2',
+            fechaInicio: new Date('2023-02-01'),
+            fechaFin: new Date('2023-11-30'),
+        },
+        ];
+    }
+}
